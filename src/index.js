84cf2174dfe0733a93f5cec9337f4d93f319c032
praviteja143 <33865933+praviteja143@users.noreply.github.com>
2018-07-22 14:34:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,21 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 import Clock from './Clock';
 
 
-<<<<<<< HEAD
 //ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<Nav/>, document.getElementById('root1'));
 ReactDOM.render(<Main />, document.getElementById("root"));
-=======
-const ele = {
-    name:new Date(),
-    l:0,
-    clock:Clock.js,
-};
-
-ReactDOM.render(<App  name= {ele.name.toDateString()} l={ele.l}/>, document.getElementById('root'));
-//ReactDOM.render(<Clock />, document.getElementById('root1'));
-
->>>>>>> 3eb7c25308a2dae1fc3e7f3b720ef945a4a64f52
 
 registerServiceWorker();
-setInterval(App, 1000);
